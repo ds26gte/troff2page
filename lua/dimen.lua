abc6ed5bd47563e61c1042394335ae271d10e5c7
Dorai Sitaram <ds26gte@yahoo.com>
2017-08-20 15:10:05 -0400

    Recognize u as a scaling indicator.

--- lua/dimen.lua
+++ lua/dimen.lua
@@ -1,3 +1,5 @@
+-- last modified 2017-08-20
+
 function point_equivalent_of(indicator)
   if indicator == 'c' then return point_equivalent_of('i')/2.54
   elseif indicator == 'i' then return 72
@@ -7,6 +9,7 @@ function point_equivalent_of(indicator)
   elseif indicator == 'n' then return point_equivalent_of('m') * .5
   elseif indicator == 'p' then return 1
   elseif indicator == 'P' then return 12
+  elseif indicator == 'u' then return 1
   else terror('point_equivalent_of: unknown indicator %s', indicator)
   end
 end
@@ -32,11 +35,11 @@ function read_length_in_pixels()
   local n = read_arith_expr()
   local u = snoop_char()
   local res
-  if u == 'c' or u == 'i' or u == 'm' or u == 'n' or u == 'p' or u == 'P' then
+  if u == 'c' or u == 'i' or u == 'm' or u == 'n' or u == 'p' or u == 'P' or u == 'u' then
     get_char(); res= math_round(n*point_equivalent_of(u))
   else
     res= math_round(4.5*n)
   end
-  --print('read_length_in_pixels retng', res)
+  --print('read_length_in_pixels ->', res)
   return res
 end 
